f42e3da95bc4057567057dc51b848c2f4c2d4d88
Camila Oliveira <94542843+camilasamurai@users.noreply.github.com>
2023-06-07 18:40:41 +0200

    alert for future issues

--- homepage.js
+++ homepage.js
@@ -61,3 +61,7 @@ function2 = () => {
   headTag.appendChild(linkforCSSfile);
   document.body.appendChild(headTag);
 };
+
+function oncomingissue(){
+  alert("This is an on coming issue. The future issues are coming down the pike!")
+}
