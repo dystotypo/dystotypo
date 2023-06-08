c658788f51fe3ec755a4e6587b23c448ccc50954
Camila Oliveira <94542843+camilasamurai@users.noreply.github.com>
2023-06-08 19:32:03 +0200

    test

--- homepage.js
+++ homepage.js
@@ -28,5 +28,5 @@ window.addEventListener("load", function() {
 });
 
 function oncomingissue(){
-  alert("This is an on coming issue. The future issues are coming down the pike!")
+  alert("The future issues are coming down the pike!")
 }
