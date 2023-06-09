62da98d4f57c00a4941e5a67e0a521f0bfba681b
Camila Oliveira <94542843+camilasamurai@users.noreply.github.com>
2023-06-09 17:03:51 +0200

    test

--- textspage.js
+++ textspage.js
@@ -96,3 +96,23 @@ function filltabs(){
 				}) )
 			}
 		}
+
+    function changeTheme(themeName) {
+      var themeLink = document.getElementById("theme-link");
+    
+      themeLink.setAttribute("href", themeName);
+      // Store the theme preference in local storage
+      localStorage.setItem("theme", themeName);
+    }
+    
+    window.addEventListener("DOMContentLoaded", function() {
+      var storedTheme = localStorage.getItem("theme");
+      if (storedTheme) {
+        var themeLink = document.getElementById("theme-link");
+        themeLink.setAttribute("href", storedTheme);
+      }
+    });
+    
+    function oncomingissue(){
+      alert("The future issues are coming down the pike!")
+    }
