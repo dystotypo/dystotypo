20946f93c747d79eeecfa5d5286f37419e86131e
Camila Oliveira <94542843+camilasamurai@users.noreply.github.com>
2023-06-11 10:17:24 +0200

    test metadata

--- textspage.js
+++ textspage.js
@@ -17,21 +17,21 @@ $(document).ready(function () {
   $('#icon1').click(function () {
         $('#text').load('1984.html');
         // $('#file').html(d);
-        $('.show').prop("checked", false);
+        //$('.show').prop("checked", false);
         filltabs();
         addIds();
   });
   $('#icon2').click(function () {
         $('#text').load('fahrenheit.html');
         // $('#file').html(d);
-        $('.show').prop("checked", false);
+        //$('.show').prop("checked", false);
         filltabs();
         addIds();
   });
   $('#icon3').click(function () {
         $('#text').load('bravenewworld.html');
         // $('#file').html(d)
-        $('.show').prop("checked", false);
+        //$('.show').prop("checked", false);
         filltabs();
         addIds();
 });
